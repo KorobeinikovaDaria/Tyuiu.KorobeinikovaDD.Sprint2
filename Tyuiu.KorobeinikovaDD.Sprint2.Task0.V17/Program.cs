1da5e553b0039590cbe9df3d875b4663fd41444d
Дарья <@LAPTOP-RSOI4V6R>
2024-09-28 19:57:37 +0500

    Add project files.

---
+++ Tyuiu.KorobeinikovaDD.Sprint2.Task0.V17/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.KorobeinikovaDD.Sprint2.Task0.V17.Lib;
+
+DataService ds = new DataService();
+
+int x = 1065;
+int y = 755;
+bool[] res = new bool[6];
+res = ds.GetCompareOperations(x, y);
+
+Console.WriteLine("******************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                               *");
+Console.WriteLine("******************************************************************");
+
+Console.WriteLine("X = " + x);
+Console.WriteLine("Y = " + y);
+
+Console.WriteLine("******************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                     *");
+Console.WriteLine("******************************************************************");
+
+for (int i=0; i<6; i++)
+{
+    Console.WriteLine(res[i]);
+}
+Console.ReadKey();
